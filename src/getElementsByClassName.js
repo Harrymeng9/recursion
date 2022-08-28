c35a37a0e1fcb6ca2d99839c125b691f5daa03cf
HarryMeng <harry.mld.2010@gmail.com>
2022-08-27 23:42:33 -0700

    Update getElementsByClassName.js and stringifyJSON.js

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,41 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+
+/**
+ *
+ * @param {*} className target class name
+ * @param {*} currNode the current node that the recursion is checking
+ * @returns return an array of nodes that match the condition
+ */
+var helper = function (className, currNode) {
+  var result = [];
+
+  // Step 1: Check when to stop the recursion
+  if (currNode.classList === undefined) {
+    return result;
+  }
+
+  // Step 2: Check whether the current node matches the condition
+  if (currNode.classList.contains(className)) {
+    result.push(currNode);
+  }
+
+  // Step 3: Recursively call this function for all the child nodes
+  if (currNode.childNodes.length > 0) {
+    _.forEach(currNode.childNodes, function(currChildNode) {
+      var currChildResult = helper(className, currChildNode);
+      result = result.concat(currChildResult);
+    });
+  }
+
+  return result;
 };
+
+var getElementsByClassName = function(className) {
+  // your code here
+  var documentBody = document.body;
+  var finalResult = helper(className, documentBody);
+  return finalResult;
+};
+
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,75 @@
 
 // but you don't so you're going to write it from scratch:
 
-var stringifyJSON = function(obj) {
+var stringifyJSON = function (obj) {
   // your code goes here
+  if (typeof obj === 'function' || obj === undefined) {
+    return undefined;
+  }
+
+  // var result = '';
+  return recursiveString(obj);
+  // result +=
+};
+
+var identifyEachCase = function (obj) {
+  if (typeof obj === 'number' || typeof obj === 'boolean') {
+    return obj.toString();
+  }
+  // if (obj === null) {
+  //   return 'null';
+  // }
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  }
+
+  return null;
+};
+
+
+var recursiveString = function (obj) {
+  var result = '';
+  if (obj === null) {
+    return 'null';
+  } else if (typeof obj === 'object' && Array.isArray(obj)) {
+    if (obj.length === 0) {
+      return '[]';
+    } else {
+      result += '[';
+      _.forEach(obj, function (item) {
+        if (typeof item === 'object') {
+          result += recursiveString(item) + ',';
+        } else {
+          result += identifyEachCase(item) + ',';
+        }
+      });
+      // Remove ',' and add ']'
+      result = result.slice(0, -1) + ']';
+    }
+  } else if (typeof obj === 'object' && !Array.isArray(obj)) {
+    if (Object.keys(obj).length === 0) {
+      return '{}';
+    } else {
+      result = '{';
+      Object.keys(obj).forEach(function (key) {
+        if (typeof obj[key] === 'function' || obj[key] === undefined) {
+          return;
+        }
+        if (typeof obj[key] === 'object') {
+          result += identifyEachCase(key) + ':' + recursiveString(obj[key]) + ',';
+        } else {
+          result += identifyEachCase(key) + ':' + identifyEachCase(obj[key]) + ',';
+        }
+      });
+      if (result.length > 1) {
+        result = result.slice(0, -1) + '}';
+      } else {
+        result += '}';
+      }
+    }
+  } else {
+    result += identifyEachCase(obj);
+  }
+
+  return result;
 };
